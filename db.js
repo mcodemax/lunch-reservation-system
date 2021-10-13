92ebfc5d35d30a78ea0dfcadb6c8de210cfa1e0e
mcodemax <mwalterjohnson7@gmail.com>
2021-10-12 21:13:34 -0700

    change db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 
 const pg = require("pg");
 
-const db = new pg.Client("postgresql:///lunchly");
+const db = new pg.Client("postgresql://postgres:myPassword@localhost:5433/lunchly");
 
 db.connect();
 
